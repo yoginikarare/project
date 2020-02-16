5cb7ddced2ebc68f91f4fbc7670c165c26a09a9c
yoginikarare <yoginirkarare@gamil.com>
2020-02-16 21:58:55 +0530

    added changes

--- admin.php
+++ admin.php
@@ -1,6 +1,11 @@
+
 <?php
-include "conn.php";
-$sql = "select * from users";
-$query = $sql->query($conn);
-$result = fetch_assoc($query);
+		$include "conn.php"
+		$sql = "SELECT * FROM users";
+		$query = $conn->query($conn);
+		if ($query->num_rows > 0) {
+			while ($row = $query->fetch_assoc()) {
+				header("location : login.php");
+			}
+		}
 ?>
